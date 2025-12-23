b572f5545b33d6668bb13dd8b4e12d419cc21b2c
Professor_Cheese <annaji.garimella6@gmail.com>
2025-12-23 09:56:07 -0500

    Add files via upload

---
+++ gear-ratio-calculator.js
@@ -0,0 +1,5 @@
+document.getElementById("calculateRatio").addEventListener("click", function () {
+    const driver = Number(document.getElementById("driverTeeth").value);
+    const follower = Number(document.getElementById("followerTeeth").value);
+    console.log(driver, follower);
+});
